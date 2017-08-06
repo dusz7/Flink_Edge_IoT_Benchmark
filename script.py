8d031e055a1e14018312dc7b8d0336322a3410e0
Talha Ghaffar <t.ghaffar@yahoo.com>
2017-08-06 11:55:51 -0400

    script.py

--- script.py
+++ script.py
@@ -17,6 +17,7 @@ sinkFile = sys.argv[3]
 #print spoutFile
 #print sinkFile
 
+
 def getInputRate(inFile):
     return getRate(inFile, 3, 5)
 
@@ -38,23 +39,23 @@ def getRate(_file, timestampIndex, msgIdIndex):
         rate = float((endMsg - startMsg)) / ((endTime-startTime)/1000)
     return rate
 
-def get_etl_topo_throughput(_file, timestampIndex, msgIdIndex):
+def get_etl_topo_throughput(_file, timestampIndex, msgIdIndex, path1, path2):
     with open(_file, "r") as f:
         firstAz = ""
         firstMq = ""
         lastAz = ""
         lastMq = ""
         for line in f:
-            if "AzureInsert" in line and firstAz == "":
+            if path1 in line and firstAz == "":
                 firstAz = line
-            if "PublishBolt" in line and firstMq == "":
+            if path2 in line and firstMq == "":
                 firstMq = line
             if firstAz != "" and firstMq != "":
                 break
         for line in f:
-            if "AzureInsert" in line:
+            if path1 in line:
                 lastAz = line
-            elif "PublishBolt" in line:
+            elif path2 in line:
                 lastMq = line
     firstAz = firstAz.split(",")
     lastAz = lastAz.split(",")
@@ -67,10 +68,10 @@ def get_etl_topo_throughput(_file, timestampIndex, msgIdIndex):
     azThroughput = float(azMsgs) / (azTime/1000)
     mqThroughput = float(mqMsgs) / (mqTime/1000)
 
-    print "ETLTopology: AzureInsert path Throughput (msgs/sec): " + str(azThroughput)
-    print "ETLTopology: MqttPublish path Throughput (msgs/sec): " + str(mqThroughput)
+    print topology_name + ": " + path1 + " path Throughput (msgs/sec): " + str(azThroughput)
+    print topology_name + ": " + path2 + " path Throughput (msgs/sec): " + str(mqThroughput)
 
-def get_etl_topo_latency(in_file, out_file, in_ts_index, in_msg_index, out_ts_index, out_msg_index):
+def get_etl_topo_latency(in_file, out_file, in_ts_index, in_msg_index, out_ts_index, out_msg_index, path1, path2):
     in_map = {}
     az_out_map = {}
     mq_out_map = {}
@@ -86,35 +87,41 @@ def get_etl_topo_latency(in_file, out_file, in_ts_index, in_msg_index, out_ts_in
     with open(out_file, "r") as f:
         for line in f:
             line_arr = line.split(",")
-            if "AzureInsert" in line:
+            if path1 in line:
                 ts = line_arr[out_ts_index]
                 msg_id = line_arr[out_msg_index]
                 az_out_map[msg_id] = ts
 
-            elif "PublishBolt" in line:
+            elif path2 in line:
                 ts = line_arr[out_ts_index]
                 msg_id = line_arr[out_msg_index]
                 mq_out_map[msg_id] = ts
     az_total_latency = 0
     mq_total_latency = 0
+    az_count = 0
+    mq_count = 0
     for key in in_map:
         start_time = long(in_map[key])
         try:   
             az_end_time = long(az_out_map[key])
             if (az_end_time):
                 az_total_latency += (az_end_time - start_time)
+                az_count+=1
         except:
             pass
         try:
             mq_end_time = long(mq_out_map[key])
             if (mq_end_time):
                 mq_total_latency += (mq_end_time - start_time)
+                mq_count+=1
         except:
             pass
-    mq_av_latency = mq_total_latency / len(mq_out_map)
-    az_av_latency = az_total_latency / len(az_out_map)
-    print "ETLTopology: AzureInsert path Latency (msec): " + str(az_av_latency)
-    print "ETLTopology: MqttPublish path Latency (msec): " + str(mq_av_latency)
+    mq_av_latency = mq_total_latency / mq_count
+    az_av_latency = az_total_latency / az_count
+    #print len(mq_out_map)
+    #print len(az_out_map)
+    print topology_name + ": " + path1 + " path Latency (msec): " + str(az_av_latency)
+    print topology_name + ": " + path2 + " path Latency (msec): " + str(mq_av_latency)
 
     
 
@@ -126,7 +133,7 @@ def getLatency(in_file, out_file, in_ts_index, in_msg_index, out_ts_index, out_m
         for line in f:
             line_arr = line.split(",")
             ts = line_arr[in_ts_index]
-            msg_id = line_arr[in_msg_index].rstrip()
+            msg_id = line_arr[in_msg_index]
             in_map[msg_id] = ts
     
     with open(out_file, "r") as f:
@@ -153,9 +160,17 @@ def getLatency(in_file, out_file, in_ts_index, in_msg_index, out_ts_index, out_m
 
 if topology_name == "ETLTopology":
     print "Input Rate (msgs/sec): " + str(getInputRate(spoutFile))
-    get_etl_topo_throughput(sinkFile, 3, 4)
-    get_etl_topo_latency(spoutFile, sinkFile, 3, 5, 3 ,4)
+    get_etl_topo_throughput(sinkFile, 3, 4, "AzureInsert", "PublishBolt")
+    get_etl_topo_latency(spoutFile, sinkFile, 3, 5, 3 ,4, "AzureInsert", "PublishBolt")
 elif  topology_name == "WordCountTopology":
+    print "Input Rate (msgs/sec): " + str(getInputRate(spoutFile))
     print "Throughput (msgs/sec): " + str(getThroughput(sinkFile))
     print "Average Latency (ms): " + str(getLatency(spoutFile, sinkFile, 3, 5, 3 ,4))
-
+elif topology_name == "iot_prediction_topology":
+    print "Input Rate (msgs/sec): " + str(getInputRate(spoutFile))
+    get_etl_topo_throughput(sinkFile, 3, 4, "DTC", "MLR")
+    get_etl_topo_latency(spoutFile, sinkFile, 3, 5, 3 ,4, "DTC", "MLR")
+elif topology_name == "stats_with_vis":
+    print "Input Rate (msgs/sec): " + str(getInputRate(spoutFile))
+    print "Throughput (msgs/sec): " + str(getThroughput(sinkFile))
+    print "Average Latency (ms): " + str(getLatency(spoutFile, sinkFile, 3, 5, 3 ,4))
